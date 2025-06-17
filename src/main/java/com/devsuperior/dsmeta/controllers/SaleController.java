cf0692e6018fce919278a459240cc93737f4a89d
Henio Junior <henioajunior@gmail.com>
2025-06-17 11:28:21 -0300

    Create getReport and getSummary method with LocalDate params

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,14 +1,18 @@
 package com.devsuperior.dsmeta.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -24,14 +28,34 @@ public class SaleController {
 	}
 
 	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
-		// TODO
-		return null;
+	public ResponseEntity<Page<SaleMinDTO>> getReport(
+			@RequestParam(name = "minDate", defaultValue = "") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate minDate,
+			@RequestParam(name = "maxDate", defaultValue = "") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate maxDate,
+			@RequestParam(name = "name", defaultValue = "") String name,
+			Pageable pageable) {
+		if(maxDate == null) {
+			maxDate = LocalDate.now(ZoneId.systemDefault());
+		}
+		if(minDate == null) {
+			minDate = maxDate.minusYears(1L);
+		}
+		Page<SaleMinDTO> sales = service.searchSalesBetweenDateForName(minDate, maxDate, name, pageable);
+		return ResponseEntity.ok(sales);
 	}
 
 	@GetMapping(value = "/summary")
-	public ResponseEntity<?> getSummary() {
-		// TODO
-		return null;
+	public ResponseEntity<Page<SaleMinDTO>> getSummary(
+			@RequestParam(name = "minDate", defaultValue = "") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate minDate,
+			@RequestParam(name = "maxDate", defaultValue = "") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate maxDate,
+			Pageable pageable
+	) {
+		if(maxDate == null) {
+			maxDate = LocalDate.now(ZoneId.systemDefault());
+		}
+		if(minDate == null) {
+			minDate = maxDate.minusYears(1L);
+		}
+		Page<SaleMinDTO> sales = service.searchSalesBetweenDate(minDate, maxDate, pageable);
+		return ResponseEntity.ok(sales);
 	}
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,13 +1,15 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class SaleService {
@@ -19,5 +21,15 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleMinDTO> searchSalesBetweenDateForName(LocalDate minDate, LocalDate maxDate, String name, Pageable pageable) {
+		Page<Sale> sales = repository.searchSalesBetweenDateForName(minDate, maxDate, name, pageable);
+	return sales.map(SaleMinDTO::new);
+	}
+
+	public Page<SaleMinDTO> searchSalesBetweenDate(LocalDate minDate, LocalDate maxDate, Pageable pageable) {
+		Page<Sale> sales = repository.searchSalesBetweenDate(minDate, maxDate, pageable);
+		return sales.map(SaleMinDTO::new);
 	}
 }
